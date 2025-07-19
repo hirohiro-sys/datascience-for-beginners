a65af26c8b89f17d05988e931361c59be3aa949f
hirokazu kato <126783940+hirohiro-sys@users.noreply.github.com>
2025-07-19 17:32:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,38 @@ train_df.dtypes
 3. データの理解・可視化
 """
 
+train_df.isnull().sum()
+# train_df.info()
+test_df.isnull().sum()
+
+# 学習用データとテストデータはまとまっている方が分析しやすいみたい。業務とかでは自前でテストデータを作る？
+df = pd.concat([train_df, test_df], ignore_index=True)
+df.shape
+df.tail()
+
+tmp = df.groupby("Sex")["PassengerId"].count()
+tmp
+
+plt.figure(figsize=(6,4))
+plt.bar(tmp.index,tmp.values)
+plt.show()
+
+tmp.plot(kind="bar", figsize=(6,4))
+pass
+
+# paletteで色付けできる
+plt.figure(figsize=(6,4))
+sns.countplot(data=df,x="Sex", palette="Set3")
+plt.show()
+
+plt.figure(figsize=(6,4))
+sns.countplot(data=df,x="Pclass")
+plt.show()
+
+plt.figure(figsize=(6,4))
+sns.countplot(data=train_df,x="Survived")
+plt.show()
+
 """
 4. データの加工・前処理
 """
